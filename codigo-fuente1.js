ec214b2f3f83cfec97302ea0ac42a52a822f3b1f
MsSeitz <michaelsebastianseitz@gmail.com>
2022-06-15 16:56:58 -0300

    Add Function

--- codigo-fuente1.js
+++ codigo-fuente1.js
@@ -1,17 +1,32 @@
+let promedio1 = 0;
+let promedio2 = 0;
+
 let nombre1 = prompt ("Ingrese al primer alumno");
-let numero1 = Number(prompt ("Ingrese primera nota"));
-let numero2 = Number(prompt ("Ingrese segunda nota"));
-let numero3 = Number(prompt ("Ingrese tercera nota"));
+let primerNumero1 = Number(prompt ("Ingrese primera nota"));
+let segundoNumero1 = Number(prompt ("Ingrese segunda nota"));
+let tercerNumero1 = Number(prompt ("Ingrese tercera nota"));
 
 let nombre2 = prompt ("Ingrese al segundo alumno");
-let numero4 = Number(prompt ("Ingrese primera nota"));
-let numero5 = Number(prompt ("Ingrese segunda nota"));
-let numero6 = Number(prompt ("Ingrese tercera nota"));
+let primerNumero2 = Number(prompt ("Ingrese primera nota"));
+let segundoNumero2 = Number(prompt ("Ingrese segunda nota"));
+let tercerNumero2 = Number(prompt ("Ingrese tercera nota"));
 
 
+function promediar1 (primerNumero1, segundoNumero1, tercerNumero1) {
+    
+    return ((primerNumero1 + segundoNumero1 + tercerNumero1)/3);
 
-let promedio1 = (((numero1 + numero2 + numero3)/3));
-let promedio2 = (((numero4 + numero5 + numero6)/3));
+}
+
+function promediar2 (primerNumero2, segundoNumero2, tercerNumero2) {
+    
+    return ((primerNumero2 + segundoNumero2 + tercerNumero2)/3);
+
+}
+
+promedio1 = promediar1 (primerNumero1, segundoNumero1, tercerNumero1);
+
+promedio2 = promediar2 (primerNumero2, segundoNumero2, tercerNumero2);
 
 
 if (promedio1 >= 7 && promedio2 >= 7) {
@@ -36,5 +51,3 @@ else {
     alert("El promedio del alumno " + nombre1 + " es " + promedio1 + " y el promedio del alumno " + nombre2 + " es " + promedio2 + ", Una lastima por ambos!!");
 
 }
-
-
